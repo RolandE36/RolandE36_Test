d0d73378eb939eca6adc308fa5ea16d1141702fb
Roman Lytvyn <Roman.Lytvyn@verint.com>
2017-01-26 16:50:23 +0200

    Added user to db

--- WebApplication1/Controllers/HomeController.cs
+++ WebApplication1/Controllers/HomeController.cs
@@ -3,10 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WebApplication1.Models;
 
 namespace WebApplication1.Controllers {
 	public class HomeController : Controller {
 		public ActionResult Index() {
+			using (var contect = new Context())
+			{
+				var user = new User() {Name = DateTime.UtcNow.ToString()};
+				contect.Users.Add(user);
+				contect.SaveChanges();
+				ViewBag.User = user;
+			}
+
 			return View();
 		}
 
