d5f230d060fa17159eecf1a827afa5100f5c3069
Roman Lytvyn <Roman.Lytvyn@verint.com>
2017-01-26 16:44:40 +0200

    Added project

---
+++ WebApplication1/Context.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using WebApplication1.Models;
+
+namespace WebApplication1 {
+	public class Context : DbContext {
+
+		public Context() : base("Def") {
+			Configuration.LazyLoadingEnabled = true;
+		}
+
+		public DbSet<User> Users { get; set; }
+	}
+}
---
+++ WebApplicationMySQL/Models/User.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebApplicationMySQL.Models {
+	public class User {
+		[Key]
+		public int StudentID { get; set; }
+		public string StudentName { get; set; }
+		public DateTime? DateOfBirth { get; set; }
+	}
+}
